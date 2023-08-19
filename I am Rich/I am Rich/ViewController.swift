d7bd83e7b263d820f1e492cb50bcaaac19b92e7b
Muhammed Melih GÜNDOĞAN <muhammedmelihgundogan@gmail.com>
2023-08-19 16:57:39 +0300

    add: I am Rich Project

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Muhammed Melih Gündoğan on 13.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
